ff1557b944410ca1c0426056a74951aea01b7e7f
Luke Tucker <ltucker@ltucker.local>
2009-10-10 11:28:43 -0400

    add a simple profile of bucket inserting

---
+++ tests/prof.py
@@ -0,0 +1,31 @@
+from melkman.green import green_init, GreenContext, consumer_loop
+green_init()
+
+from helpers import *
+from melkman.db import NewsBucket, RemoteFeed
+
+
+def profile_bucket_saves():
+    ctx = fresh_context()
+    
+    import hotshot
+    prof = hotshot.Profile("bucket_saves.prof")
+    prof.runcall(_profile_bucket_saves, ctx)
+    prof.close()
+
+def _profile_bucket_saves(context):
+    bucket = NewsBucket.create(context)
+    for i in range(100):
+        for i in range(100):
+            bucket.add_news_item(dummy_news_item({}))
+        bucket.save()
+        
+def profile_push_index(context):
+    pass
+    
+if __name__ == '__main__':
+    import sys
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+    
+    profile_bucket_saves()
