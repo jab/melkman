d59eb8716eabdfdd6a0e4a568177d5c53f9bbae5
Luke Tucker <ltucker@ltucker.local>
2009-10-10 21:55:30 -0400

    add support for 'with' to context

--- melkman/context.py
+++ melkman/context.py
@@ -53,6 +53,13 @@ class Context(object):
             self.broker.close()
             del self._local.broker
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+        return False
+
     #######################
     # Database
     #######################
