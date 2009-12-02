jab/melkman

commit 4a345df6ea5920e88211c6cc7e23eb89770de073

must raise an Exception not a string in python >= 2.6
from http://www.python.org/dev/peps/pep-0008/:

      String exceptions in new code are forbidden, because this language
      feature is being removed in Python 2.6.

melkman/db/bucket.py
@@ -320,7 +320,7 @@ class NewsBucket(DocumentHelper):
             self._entries = None
 
         if not main_doc_saved:
-            raise main_doc_result
+            raise Exception(main_doc_result)
 
 
     def _send_modified_event(self, *args, **kw):

commit b6ba966dd1e3a269a1ff3dc4f1511bfddb384205

hardcode sortkey as attrgetter('timestamp') for now
fix regression caused by commit 4a345df6ea5920e88211c6cc7e23eb89770de073
(i incorrectly assumed main_doc_result was not an Exception, but it in
fact is a ResourceConflict)

melkman/db/bucket.py
@@ -22,21 +22,20 @@ from couchdb.design import ViewDefinition
 from couchdb.schema import *
 from datetime import datetime
 from functools import wraps
-import logging
 from melk.util.nldict import nldict
 from melk.util.nonce import nonce_str
 from melk.util.hash import melk_id
-
 from melkman.db.util import DocumentHelper, MappingField, DibjectField
 from melkman.aggregator.api import notify_bucket_modified
+from operator import attrgetter
+import logging
+log = logging.getLogger(__name__)
 
 __all__ = ['NewsItem', 'NewsBucket',
            'immediate_add',
            'view_entries',
            'view_entries_by_timestamp',
            'view_entries_by_add_time']
-
-log = logging.getLogger(__name__)
 
 
 class NewsItem(DocumentHelper):
@@ -115,6 +114,7 @@ class NewsItemRef(DocumentHelper):
         return '%s_%s' % (bucket_id, item_id)
 
 
+SORTKEY = attrgetter('timestamp')
 class NewsBucket(DocumentHelper):
 
     document_types = ListField(TextField(), default=['NewsBucket'])
@@ -135,7 +135,6 @@ class NewsBucket(DocumentHelper):
         self._removed = {}
         self._updated = {}
         self._maxlen = kw.get('maxlen')
-        self._sortkey = kw.get('sortkey')
 
     def __len__(self):
         return len(self.entries)
@@ -163,7 +162,7 @@ class NewsBucket(DocumentHelper):
         @wraps(method, ('__name__', '__doc__'))
         def wrapper(self, *args, **kwds):
             if force or self._entries is None:
-                self._entries = nldict(self._maxlen, self._sortkey)
+                self._entries = nldict(self._maxlen, SORTKEY)
                 self._entries.observers.append(self)
                 
                 # saved in db
@@ -320,7 +319,8 @@ class NewsBucket(DocumentHelper):
             self._entries = None
 
         if not main_doc_saved:
-            raise Exception(main_doc_result)
+            assert isinstance(main_doc_result, ResourceConflict)
+            raise main_doc_result
 
 
     def _send_modified_event(self, *args, **kw):